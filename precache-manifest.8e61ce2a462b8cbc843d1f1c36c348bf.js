b07456fbef6e02d49b94c2348885b3c50b4db1c7
Ekaterina <ekaterinan112@gmail.com>
2020-11-03 13:18:01 +0300

    Updates

---
+++ precache-manifest.8e61ce2a462b8cbc843d1f1c36c348bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9a3d6aef712bdcf04f6bb7bb6247f7",
+    "url": "/counter-with-redux/index.html"
+  },
+  {
+    "revision": "d44a2c82d947ca176d0c",
+    "url": "/counter-with-redux/static/css/main.eeb76101.chunk.css"
+  },
+  {
+    "revision": "7da39f3415ac51b8cab2",
+    "url": "/counter-with-redux/static/js/2.05941427.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/counter-with-redux/static/js/2.05941427.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d44a2c82d947ca176d0c",
+    "url": "/counter-with-redux/static/js/main.ced6dc82.chunk.js"
+  },
+  {
+    "revision": "ee4ced85287d27da6458",
+    "url": "/counter-with-redux/static/js/runtime-main.81a0f510.js"
+  },
+  {
+    "revision": "ba55fe4d6e642b12c975f8b0bb0a6104",
+    "url": "/counter-with-redux/static/media/back.ba55fe4d.jpg"
+  }
+]);
